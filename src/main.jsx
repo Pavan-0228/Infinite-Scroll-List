f6b9e8df572e6ddf2160fd48b8423ec4091bcda6
Pavan Rasal <140827035+Pavan-0228@users.noreply.github.com>
2024-09-09 16:07:09 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // Adjust the path if needed
-import { ThemeProvider } from './ThemeContext'; // Adjust the path if needed
-import './styles.css'; // Import your global styles
+import App from './App'; 
+import { ThemeProvider } from './ThemeContext'; 
+import './styles.css'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
